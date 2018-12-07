ddb6913227d348fc5513ca216ba1b6d9a67fc49a
zsm <shanmin.zhang@dena.com>
2018-12-07 18:06:45 +0800

    .DS_Store

---
+++ webflux/src/main/java/com/denachina/webflux/WebfluxApplication.java
@@ -0,0 +1,34 @@
+package com.denachina.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+/**
+ * HealthController
+ *
+ * @author shanmin.zhang
+ * @date 18/12/6
+ **/
+
+@SpringBootApplication
+public class WebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxApplication.class, args);
+    }
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter() {
+            @Override
+            public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+                return chain.filter(exchange);
+            }
+        };
+    }
+}

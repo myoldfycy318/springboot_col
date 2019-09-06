a2cc139c76718f8f7bd12cf1a47f2f47a32927eb
shanmin.zhang <shanmin.zhang@dena.com>
2019-09-06 19:34:19 +0800

    kafka广播消费

--- springboot-pure/src/main/java/com/SpringbootPureApplication.java
+++ springboot-pure/src/main/java/com/SpringbootPureApplication.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class SpringbootPureApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootPureApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.setProperty("appconfig.customerTopic", "test" + UUID.randomUUID().toString());
+        SpringApplication.run(SpringbootPureApplication.class, args);
+    }
 }

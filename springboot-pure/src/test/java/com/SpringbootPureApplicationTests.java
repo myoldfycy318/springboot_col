0eee878da02a7621e5b9e8e81db3e7ea929fd2a9
shanmin.zhang <shanmin.zhang@dena.com>
2019-09-25 21:48:03 +0800

    redis

--- springboot-pure/src/test/java/com/SpringbootPureApplicationTests.java
+++ springboot-pure/src/test/java/com/SpringbootPureApplicationTests.java
@@ -1,16 +1,59 @@
 package com;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class SpringbootPureApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
+
+
+    @Autowired
+    RedisTemplate<String, String> redisTemplate;
+
+
+    @Test
+    public void test() {
+        redisTemplate.opsForValue().increment("key", 1);
+        Object result = redisTemplate.opsForValue().get("key");
+        redisTemplate.opsForHash().increment("hash", "a", 1);
+        log.error("-->" + result);
+        result = redisTemplate.opsForHash().get("hash", "a");
+        log.info("2--->" + result);
+    }
+
+
+    @Test
+    public void test1() throws InterruptedException {
+
+        CountDownLatch downLatch = new CountDownLatch(500);
+
+        for (int i = 0; i < 500; i++) {
+
+            CompletableFuture.runAsync(() -> {
+                redisTemplate.opsForValue().increment("key", 1);
+                downLatch.countDown();
+            });
+
+        }
+        downLatch.await();
+        Object result = redisTemplate.opsForValue().get("key");
+        log.info("2--->" + result);
+    }
+
 
 }

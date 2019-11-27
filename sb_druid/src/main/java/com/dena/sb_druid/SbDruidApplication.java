ae27732c41d168208dce3cf2dc1aabdda49a5b31
shanmin.zhang <shanmin.zhang@dena.com>
2019-11-27 19:34:54 +0800

    springboot项目优雅的停止服务

--- sb_druid/src/main/java/com/dena/sb_druid/SbDruidApplication.java
+++ sb_druid/src/main/java/com/dena/sb_druid/SbDruidApplication.java
@@ -1,16 +1,77 @@
 package com.dena.sb_druid;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.connector.Connector;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextClosedEvent;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 
 @SpringBootApplication
 @ServletComponentScan
 @MapperScan({"com.dena.sb_druid.dao"})
+@Slf4j
 public class SbDruidApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SbDruidApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SbDruidApplication.class, args);
+    }
+
+
+    /**
+     * springboot项目优雅的停止服务
+     *
+     * 用于接受 shutdown 事件
+     */
+    @Bean
+    public GracefulShutdown gracefulShutdown() {
+        return new GracefulShutdown();
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+        tomcatServletWebServerFactory.addConnectorCustomizers(gracefulShutdown());
+        return tomcatServletWebServerFactory;
+    }
+
+    private class GracefulShutdown implements TomcatConnectorCustomizer, ApplicationListener<ContextClosedEvent> {
+        private volatile Connector connector;
+        private final int waitTime = 10;
+
+        @Override
+        public void customize(Connector connector) {
+            this.connector = connector;
+        }
+
+        @Override
+        public void onApplicationEvent(ContextClosedEvent contextClosedEvent) {
+            this.connector.pause();
+            Executor executor = this.connector.getProtocolHandler().getExecutor();
+            try {
+                if (executor instanceof ThreadPoolExecutor) {
+                    ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) executor;
+                    threadPoolExecutor.shutdown();
+                    if (!threadPoolExecutor.awaitTermination(waitTime, TimeUnit.SECONDS)) {
+                        log.warn("Tomcat 进程在" + waitTime + " 秒内无法结束，尝试强制结束");
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+
+    }
 }
